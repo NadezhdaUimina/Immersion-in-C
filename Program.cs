a66c0b711b35c66aadc74042fb78084abfaef16c
nadezhda <nadezhda.bessonova@gmail.com>
2023-11-27 12:56:06 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.Design;
+
+namespace homework1
+{
+    internal class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 3)
+            {
+               string[] newArray = new string[args.Length];
+               int j = 0;
+
+                if (args[1] == "*")
+                {
+                    Console.WriteLine($"{args[0]} * {args[2]} = {int.Parse(args[0]) * int.Parse(args[2])}");
+                }
+
+                if (args[1] == "/")
+                {
+                    Console.WriteLine($"{args[0]} / {args[2]} = {int.Parse(args[0]) / int.Parse(args[2])}");
+                }
+
+                if (args[1] == "+")
+                {
+                    Console.WriteLine($"{args[0]} + {args[2]} = {int.Parse(args[0]) + int.Parse(args[2])}");
+                }
+
+                if (args[1] == "-")
+                {
+                    Console.WriteLine($"{args[0]} - {args[2]} = {int.Parse(args[0]) - int.Parse(args[2])}");
+                }
+         
+            }
+            else
+            {
+                Console.WriteLine("Need args");
+            }
+        }
+    }
+}
